1d28c34679e9b2530856e7117a0616b2a75effc1
hale-bopp97 <mattastik@gmail.com>
2022-10-26 07:36:59 -0700

    reaction POST and DELETE added to thought-routes

--- models/Reaction.js
+++ models/Reaction.js
@@ -15,7 +15,7 @@ const reactionSchema = new Schema(
         },
         username: {
             type: String,
-            required:true
+            required: true
         },
         createdAt: {
             type: Date,
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,10 +1,9 @@
-//1:35:55
 const router = require('express').Router();
 const { User, Thought } = require('../../models');
 
 // api/thoughts
 router.get('/', async (req, res) => {
-    console.log('hit / routes for thoughts');
+    //console.log('hit / routes for thoughts');
     try {
 
         const dbThoughtData = await Thought.find().sort({ createdAt: -1 });
@@ -20,7 +19,7 @@ router.get('/', async (req, res) => {
 
 // api/thoughts/:thoughtId
 router.get('/:thoughtId', async (req, res) => {
-    console.log(req.params.thoughtId);
+    //console.log(req.params.thoughtId);
     try {
         console.log(req.params.thoughtId);
         const dbThoughtData = await Thought.findOne(
@@ -124,6 +123,65 @@ router.delete('/:thoughtId', async (req, res) => {
         res.status(500).json(err);
 
     }
+
+});
+
+// /api/thoughts/:thoughtId/reactions
+router.post('/:thoughtId/reactions', async (req, res) => {
+
+    try {
+
+        const dbThoughtData = await Thought.findOneAndUpdate(
+
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body }},
+            { new: true }
+
+        );
+
+        if(!dbThoughtData) {
+
+            return res.status(404).json({ message: `No thought found with that ID` });
+
+        }
+
+        res.status(200).json(dbThoughtData);
+
+    } catch (err) {
+
+        res.status(500).json(err);
+
+    }
+
+});
+
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+
+    try {
+
+        const dbThoughtData = await Thought.findOneAndUpdate(
+
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId }}},
+            { new: true}
+
+        );
+
+        if (!dbThoughtData) {
+
+            return res.status(404).json({ message: `No reaction with that ID` });
+
+        }
+
+
+        res.status(200).json(dbThoughtData);
+
+    } catch (err) {
+
+        res.status(500).json(err);
+
+    }
+
 
 });
 
